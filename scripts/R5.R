fb67fa81e8e7e585118868a588049106cf37da5e
Serhat Aktay <61043167+SerhatAktay@users.noreply.github.com>
2023-12-12 15:03:36 +0100

    Update R5.R
    Update to force installation of packages if required

--- scripts/R5.R
+++ scripts/R5.R
@@ -9,6 +9,10 @@ sample_conditions <- args[conditions_to_extract]
 
 ###########
 
+list.of.packages <- c("dplyr", "gtools", "DESeq2", "ggrepel", "ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(DESeq2)
 library(dplyr)
 library(ggrepel)
